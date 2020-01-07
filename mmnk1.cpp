92a9039a427fc02b1fd1d40b4cd56b3ac2a73c57
knzkhuka <one235813@gmail.com>
2020-01-07 23:43:15 +0900

    ?

--- mmnk1.cpp
+++ mmnk1.cpp
@@ -7,11 +7,31 @@ constexpr ll MOD = (1e9+7);
 template<class T>vector<T> vec(size_t a){return vector<T>(a);}
 template<class T, class... Ts>auto vec(size_t a, Ts... ts){return vector<decltype(vec<T>(ts...))>(a, vec<T>(ts...));}
 
+template<ll MOD=ll(1e9+7)>
+ll mod_pow(ll n,ll p){
+  ll ans=1LL;
+  while(p){
+    if(p&1)ans*=n;
+    n*=n;
+    p>>=1;
+    ans%=MOD;
+    n%=MOD;
+  }
+  return ans;
+}
+
 
 int main(){
+  int n,b,k;
+  cin>>n>>b>>k;
+  ll ans = mod_pow(b,k);
+  ans -= mod_pow(b-1,k);
+  cout<<(ans)<<endl;
+}
 
+int _main(){
 
-  // n桁,b進数,kになる
+  // n桁,b進数,kが現れる
   int n,b,k;
   cin>>n>>b>>k;
 
